d0f6665be9cb9d08ab326e4d12c2ca5b7b65532c
Becca Barton <hello@beccabarton.com>
2016-06-27 16:10:16 -0400

    first working version of d3

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,7 +1,7 @@
 class Plate < ActiveRecord::Base
   class << self
-    def group_by_date
-      all.group_by{ |plate| plate.date.strftime("%B") }
+    def group_by_month
+      all.sort_by(&:date_by_month).group_by(&:date_by_month)
     end
 
     def plates_content
@@ -9,17 +9,21 @@ class Plate < ActiveRecord::Base
     end
 
     def structured_for_api
-      plates = all.group_by{ |plate| plate.date.strftime("%B") }
+      plates = all.sort_by(&:date_by_month).group_by(&:date_by_month)
       restructured_plates = []
       plates.each do | month_key, all_plates_value |
         new_month = {}
-        new_month[:month] = month_key
+        new_month[:month] = Date::MONTHNAMES[month_key.to_i]
         new_month[:count] = all_plates_value.count
         new_month[:plates_content] = all_plates_value
         restructured_plates << new_month
       end
       restructured_plates
     end
+  end
+
+  def date_by_month
+    self.date.strftime("%m")
   end
 
   def date_by_day
